ab6cef3d17740244c7afe90a1220c8568f92d705
Luis Felipe S Botta <felipeex7@gmail.com>
2024-03-12 11:27:13 -0300

    1

---
+++ tipos_de_dados.py
@@ -0,0 +1,11 @@
+print(1 + 10)
+print(1.1 + 10.1)
+print(True)
+print(False)
+print("Python")
+
+
+int()
+float()
+str()
+bool()
---
+++ variaveis_constantes.py
@@ -0,0 +1,13 @@
+nome = "Felipe"
+idade = 24
+
+nome, idade= "Luis", 25
+
+print(nome,idade)
+
+limite_saque_diario = 1000
+
+
+STATES = ["SP", "SC", "PR", "RS"]
+
+print(STATES)
